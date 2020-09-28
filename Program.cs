7c4c7f8ef3503d4ba00291a8b52d09a2daee528d
tejaswishah1 <tejaswishah1998@gmail.com>
2020-09-28 18:33:26 +0530

    [tejas] add message Welcome to the game

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakesAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the game of Snakes and Ladders");
+        }
+    }
+}
