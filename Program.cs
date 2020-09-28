a2e1b0d30b4743441a17eacc4de7c689b82243a5
tejaswishah1 <tejaswishah1998@gmail.com>
2020-09-28 18:42:09 +0530

    [tejas] add message Number on dice

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@ namespace SnakesAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the game of Snakes and Ladders");
+            //// Starting position of player
+
+            int position = 0;
+            Console.WriteLine("Start Position of Player 1 = 0");
+
+            Random random = new Random();
+            ////Generating value on a dice
+            int dice = random.Next(1, 7); //// Dice will choose a number between 1 to 6
+
+            
+            Console.WriteLine("The number on dice =" + dice);
         }
     }
 }
